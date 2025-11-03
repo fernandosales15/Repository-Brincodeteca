0dbc3659e639d19c2b6d34bb847707084b408128
Fernando Sales <nandolassance@icloud.com>
2025-11-03 20:13:04 -0300

    Addd

--- Associate_Software_Engineer/JavaScript/09_Loops/loopFor.js
+++ Associate_Software_Engineer/JavaScript/09_Loops/loopFor.js
@@ -1,6 +1,10 @@
+//Loops
+console.log("---- Exercicio de Loops: Contando Idade ---- ")
 let idade = 0; 
 
 for (i = 0; i <= 1000; i++) {
     console.log(`Idade atual: ${idade}`);
-    idade += 100;
+    console.log(`Adicionando + 1 ano a idade`);
+    idade += 1;
 }
+    console.log(`Final do Loop - A idade atingiu a condição esperada`);
