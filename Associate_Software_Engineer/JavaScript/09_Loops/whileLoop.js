498749e9716cc4f42887e6a3b9df89baaa44bafe
Fernando Sales <nandolassance@icloud.com>
2025-11-04 21:39:16 -0300

    While Looop

---
+++ Associate_Software_Engineer/JavaScript/09_Loops/whileLoop.js
@@ -0,0 +1,13 @@
+//Pratica do Laço While. 
+
+let transformNumber = 5000;
+console.log("---- Exercicio de Loops: Usando While ----");
+
+while (transformNumber >= 2) {
+    console.log(`Número atual: ${transformNumber}`);
+    transformNumber -= 1;
+}
+
+console.log(`O resultado final é o esperado: ${transformNumber}`);
+
+console.log("---- Fim do Exercicio ---- ");
