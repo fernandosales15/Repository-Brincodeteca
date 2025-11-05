c65a7774f62842c5ff01f29a24da5a0fd3256df5
Fernando Sales <nandolassance@icloud.com>
2025-11-04 21:07:59 -0300

    Valores deduzidos por Loop

--- Associate_Software_Engineer/JavaScript/09_Loops/loopFor.js
+++ Associate_Software_Engineer/JavaScript/09_Loops/loopFor.js
@@ -8,3 +8,17 @@ for (i = 0; i <= 1000; i++) {
     idade += 1;
 }
     console.log(`Final do Loop - A idade atingiu a condição esperada`);
+
+
+console.log("---- Fim do Exercicio ---- ")
+
+console.log("---- Iniciando Novo Exercício de Loops - Subtraindo Valores")
+
+let valorDeduzível = 1000; 
+
+for (i = 0; i <= 1000; i++) {
+    console.log(`Valor atual: ${valorDeduzível}`);
+    valorDeduzível -= 1;
+}
+
+console.log("---- Fim do Exercicio ---- ") 
