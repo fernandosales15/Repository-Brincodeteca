402d68aa2c7600fd01c87d85336af599132a8fd3
Fernando Sales <nandolassance@icloud.com>
2025-11-15 16:46:40 -0300

    Ajuste de branch

---
+++ Associate_Software_Engineer/JavaScript/09_Loops/nestedLoops.js
@@ -0,0 +1,12 @@
+//Praticando: Nested Loops (Loops Aninhados)
+console.log(` ---- Exerçicio de Loops: Prática de Loops Aninhados ---- `);
+
+for (let i = 1; i < 100; i++) {
+    console.log(`Inicio do Primeiro Loop: i = ${i}`);
+    for (let j = 1; j < 200; j++) {
+        console.log(` Loop Aninhado: j = ${j}`);
+    }
+}
+
+console.log(` ---- Fim do Exerçicio de Loops: Prática de Loops Aninhados ---- `);
+//
