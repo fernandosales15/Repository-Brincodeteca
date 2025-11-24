14d120b135ad06eec8e8f85b46ba93917f094475
Fernando Sales <nandolassance@icloud.com>
2025-11-24 19:25:25 -0300

    Adiciona arquivo 'functionExpression.js'

---
+++ Associate_Software_Engineer/JavaScript/10_Functions/functionExpression.js
@@ -0,0 +1,13 @@
+//Exerçicio de 'Expressão de Função'
+
+console.log(
+  `--- Iniciando o Exercício de Expressão de Função em JavaScript ---`
+);
+
+const idadeEmDias = function (idade) {
+  return idade * 365;
+};
+
+console.log(`A sua idade em dias é: ${idadeEmDias(33)} dias `);
+
+console.log(`--- Fim do Exercício de Expressão de Função em JavaScript ---`);
