2500482314177afaf886e4cb0ee31e1e56586819
Fernando Sales <nandolassance@icloud.com>
2025-11-26 18:45:11 -0300

    Adiciona o conceito de Arrow Functions

---
+++ Associate_Software_Engineer/JavaScript/10_Functions/arrowFunctions.js
@@ -0,0 +1,19 @@
+//Aplicando conhecimento sobre funções de seta (arrow functions)
+
+console.log(
+  `---- Iniciando um Exercício de Funções de Seta em JavaScript (Arrow Functions --- `
+);
+
+const somar = (a, b) => a + b;
+const subtrair = (c, d) => c - d;
+const multiplicar = (e, f) => e * f;
+const dividir = (g, h) => g / h;
+
+console.log(`O resultado da soma é: ${somar(500, 222)}`);
+console.log(`O resultado da subtração é: ${subtrair(1900, 750)}`);
+console.log(`O resultado da multiplicação é: ${multiplicar(25, 4)}`);
+console.log(`O resultado da divisão é: ${dividir(144, 12)}`);
+
+console.log(
+  `---- Fim do Exercício de Funções de Seta em JavaScript (Arrow Functions --- `
+);
