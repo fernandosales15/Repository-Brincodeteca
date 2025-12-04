cee2ceb5be3db145425715ed9eefe1f10356cebd
Fernando Sales <nandolassance@icloud.com>
2025-12-04 19:30:54 -0300

    Adiciona a pasta e o arquivo arrayBasic.js

---
+++ Associate_Software_Engineer/JavaScript/11_Arrays/arrayBasic.js
@@ -0,0 +1,11 @@
+//'Arrays' é uma lista de elementos, podem ser de muitos tipos diferentes (números, strings, objetos, etc)
+
+console.log(`--- Iniciando a criação de um array ---`);
+
+let listaDeFrutas = ["Maçã", "Banana", "Laranja", "Manga"];
+console.log(`--- Array criado: ---`);
+console.log(`--- imprimindo o array no console ---`);
+console.log(listaDeFrutas);
+console.log(`--- Finalizando a criação do array de frutas --- `);
+
+
