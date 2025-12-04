373a7344dc6312d70944adba2e1993662ce7cb8b
Fernando Sales <nandolassance@icloud.com>
2025-12-04 19:55:25 -0300

    Adiciona um for a oarray

--- Associate_Software_Engineer/JavaScript/11_Arrays/arrayBasic.js
+++ Associate_Software_Engineer/JavaScript/11_Arrays/arrayBasic.js
@@ -8,4 +8,14 @@ console.log(`--- imprimindo o array no console ---`);
 console.log(listaDeFrutas);
 console.log(`--- Finalizando a criação do array de frutas --- `);
 
+console.log(`-- Acessando o primeiro elemento do meu array --- `);
+console.log(listaDeFrutas[0]);
 
+console.log(
+  `--- Iniciando um laço de repetição para exibir os elementos no console ---`
+);
+for (let i = 0; i < listaDeFrutas.length; i++) {
+  console.log(`Fernando, atualmente você tem a fruta: ${listaDeFrutas[i]}`);
+}
+
+console.log(`--- Fernando, as frutas acabaram --- `);
